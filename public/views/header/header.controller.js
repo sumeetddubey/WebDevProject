a7eb077dae77307bd9f9c7fc22e614720bd9bfaf
rohitbegani <rbegani72@gmail.com>
2016-03-11 21:52:11 -0500

    Updating header links

--- public/views/header/header.controller.js
+++ public/views/header/header.controller.js
@@ -1,3 +1,21 @@
 /**
  * Created by rohitbegani on 3/10/16.
  */
+(function () {
+    "use strict";
+    angular
+        .module("codingTutorial")
+        .controller("HeaderController", HeaderController);
+
+    HeaderController.$inject = ['$scope', '$rootScope', '$location'];
+
+    function HeaderController($scope, $rootScope, $location) {
+        $scope.logout = logout;
+        $scope.location = $location;
+
+        function logout() {
+            $rootScope.user = null;
+            $location.url("/");
+        }
+    }
+})();
