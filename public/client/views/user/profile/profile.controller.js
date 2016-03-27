f7b41748a4d79f6b1b5e456edb502224fbfb8ff2
Sumeet Dubey <sumeetddubey@gmail.com>
2016-03-26 23:32:51 -0400

    minor changes

--- public/client/views/user/profile/profile.controller.js
+++ public/client/views/user/profile/profile.controller.js
@@ -5,10 +5,15 @@
     var app = angular.module("codingTutorial");
     app.controller("ProfileController", ProfileController);
 
-    function ProfileController($scope, $rootScope){
-        $scope.currentUser = $rootScope.currentUser;
-        $scope.username = $rootScope.currentUser.username;
-        console.log($scope.currentUser.image);
+    function ProfileController($scope, $rootScope, $location){
+        if($rootScope.currentUser) {
+            $scope.currentUser = $rootScope.currentUser;
+            $scope.username = $rootScope.currentUser.username;
+            console.log($scope.currentUser.image);
+        }
+        else{
+            $location.url("/home");
+        }
     }
 
 
