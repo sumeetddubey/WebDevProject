f389327104f7db5df6a25ff7eb4fda85e365addb
rohitbegani <rbegani72@gmail.com>
2016-03-27 23:14:03 -0400

    Added graph

--- public/client/app.js
+++ public/client/app.js
@@ -2,7 +2,7 @@
  * Created by sumeetdubey on 3/10/16.
  */
 (function(){
-    var app = angular.module('codingTutorial',['ngRoute', 'ngMaterial']);
+    var app = angular.module('codingTutorial',['ngRoute', 'ngMaterial', 'chart.js']);
     //angular.module('firstModule',['moduleA','moduleB']);
     //.module("codingTutorial", ["ngMaterial"]);
 })();
